19f512296646602037f6424c022211f4403b3ba5
Verdemese <jaso26@hanmail.net>
2019-06-11 00:15:44 +0900

    kermit

---
+++ day-3_css_variable/css variable.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('input');
+
+function whenBarMoving(event) {
+  if(this === event.target && this.dataset.px){
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + 'px')
+  } else {
+      document.documentElement.style.setProperty(`--${this.name}`, this.value)
+  }
+}
+
+inputs.forEach(input => input.addEventListener('input', whenBarMoving));
