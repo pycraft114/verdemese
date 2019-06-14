c353a04c709a5e5aa6c5e5580f7dc22424b359a7
Verdemese <jaso26@hanmail.net>
2019-06-15 04:08:34 +0900

    Kermit

---
+++ day-5_Search_cities/script.js
@@ -0,0 +1,25 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint)
+  .then(response => response.json())
+  .then(data => cities.push(...data));
+
+function getCitiesWhenInput() {
+  const filtered = cities.filter(place => {
+    const regex = new RegExp (this.value, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
+  })
+
+  const addToSuggestions = document.querySelector('.suggestions');
+  addToSuggestions.innerHTML = filtered.map(place =>
+    `<li>
+      <span>${place.city}, ${place.state}</span>
+      <span>${place.population}</span>
+    </li>`).join('');
+    //join method를 사용하여, 하나의 string으로 만듦.
+}
+
+
+document.querySelector('.search').addEventListener('input', getCitiesWhenInput);
---
+++ day-7 Checking box using shift key/script.js
@@ -0,0 +1,27 @@
+const checkBoxes = document.querySelectorAll('.item [type=checkbox]');
+let inBetween = false;
+let withOutShift;
+
+//체크박스를 클릭하고, 쉬프트 키를 누른 상태에서 클릭한 체크박스사이의 모든 체크박스를 체크되도록 함.
+function getCheckBoxesChecked (event) {
+  if (event.shiftKey && this.checked){
+    checkBoxes.forEach(checkBox => {
+      console.log(checkBox);
+      if (checkBox === this || checkBox === withOutShift){
+        inBetween = !inBetween;
+        console.log(1); // 1을 포함하고, 1과 1사이에 있는 checkbox 들은 체크되어야 함.
+      }
+      if (inBetween){
+        checkBox.checked = true;
+      }
+    })
+  }
+  withOutShift = this;
+}
+
+// 10번 라인의 checkBox === this 와 checkBox === withOutShift 의 차이점을 이해할 수 없습니다.
+// 그리고 withOutShift를 함수의 앞 부분(7번 라인보다 먼저) 두면 제대로 동작하지 않습니다.
+ 
+
+
+checkBoxes.forEach(checkBox => checkBox.addEventListener('click', getCheckBoxesChecked))
