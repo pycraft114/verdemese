ea0c2d6a90fd26c98f25bf64ca3b61c0e9ae45e1
Verdemese <jaso26@hanmail.net>
2019-06-19 18:22:02 +0900

    Kermit

--- day-7 Checking box using shift key/script.js
+++ day-7 Checking box using shift key/script.js
@@ -1,22 +1,20 @@
 const checkBoxes = document.querySelectorAll('.item [type=checkbox]');
 let inBetween = false;
-let withOutShift;
+let clickWithNoShift;
 
 //체크박스를 클릭하고, 쉬프트 키를 누른 상태에서 클릭한 체크박스사이의 모든 체크박스를 체크되도록 함.
-function getCheckBoxesChecked (event) {
+function getCheckBoxesChecked(event) {
   if (event.shiftKey && this.checked){
     checkBoxes.forEach(checkBox => {
-      console.log(checkBox);
-      if (checkBox === this || checkBox === withOutShift){
+      if (checkBox === this || checkBox === clickWithNoShift){
         inBetween = !inBetween;
-        console.log(1); // 1을 포함하고, 1과 1사이에 있는 checkbox 들은 체크되어야 함.
       }
       if (inBetween){
         checkBox.checked = true;
       }
     })
   }
-  withOutShift = this;
+  clickWithNoShift = this;
 }
 
 // 10번 라인의 checkBox === this 와 checkBox === withOutShift 의 차이점을 이해할 수 없습니다.
@@ -24,4 +22,4 @@ function getCheckBoxesChecked (event) {
 
 
 
-checkBoxes.forEach(checkBox => checkBox.addEventListener('click', getCheckBoxesChecked))
+checkBoxes.forEach(checkBox => checkBox.addEventListener('click', getCheckBoxesChecked));
---
+++ day-9 Key sequence detection/readme.md
@@ -0,0 +1,4 @@
+## 배운 것
+
+* splice() 의 음수 parameter를 사용했을 때 array의 뒤 쪽에서 부터 index를 인식한다는 점.
+* keys.length - keyCode.length , 결과값 만큼의 갯수를 splice() 하여, 새로 입력되는 요소만 남을 수 있도록 함.
